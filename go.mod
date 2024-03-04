560cda11e4974c68c3b26b12c5a03066e14bd701
roy <abc7468@naver.com>
2024-03-05 01:29:07 +0900

    add : test 코드 추가

---
+++ go.mod
@@ -0,0 +1,3 @@
+module language_monkey
+
+go 1.22.0
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -101,3 +101,39 @@ let result = add(five, ten);
 	}
 
 }
+
+func TestNextToken_Ex(t *testing.T) {
+	input := `
+	!-/*5;
+	5 < 10 > 5;
+`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
